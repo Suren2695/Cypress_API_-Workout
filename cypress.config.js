358ad79e92dfc7c803054742c2631d62b4d7c498
Suren2695 <surensura46@gmail.com>
2023-09-02 22:55:56 +0530

    Install cypress

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: '4k63u6',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/test/Login.cy.js
@@ -0,0 +1,18 @@
+
+// describe('Login page of EMPLOYEES PROVIDENT FUND ORGANISATION, INDIA', () =>{
+//     it('Login to the EPF', ()=>{
+//         cy.visit('https://unifiedportal-mem.epfindia.gov.in/memberinterface/');
+//         cy.get('.col-md-3').type('101182437092')
+//         cy.get('#password').type('Test123')
+//     })
+// })
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(true)
+    })
+  })
+  describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(false)
+    })
+  })

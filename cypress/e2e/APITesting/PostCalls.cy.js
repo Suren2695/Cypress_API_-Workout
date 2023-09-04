81463a2ed525740d521504dd1cd44ee4ef7b15f7
Suren2695 <surensura46@gmail.com>
2023-09-05 00:02:31 +0530

    Post call AP2

--- cypress/e2e/APITesting/PostCalls.cy.js
+++ cypress/e2e/APITesting/PostCalls.cy.js
@@ -1,10 +1,11 @@
 describe('Post Call api testing',() =>{
 
     //Approach 1
-    
+
     it('Approach 1 - Hard Coded json object', () =>{
 
         const requestBody = {
+            // YOU NEED TO CHANGE each and everytime email.
                             tourist_name : "surender",
                             tourist_email: "surenden07@gmail.com",
                             tourist_location: "CHENNAI"
@@ -20,6 +21,28 @@ describe('Post Call api testing',() =>{
             expect(res.body.tourist_email).to.eq('surenden07@gmail.com')
             expect(res.body.tourist_location).to.eq('CHENNAI')
             
+        })
+    })
+
+    //Approach 2
+
+    it.only('Approach 2 -  Dynamically generating Json objects',() =>{
+
+        const requestBody = {
+            tourist_name : Math.random().toString(5).substring(2),
+            tourist_email: Math.random().toString(5).substring(2)+'gmail.com',
+            tourist_location: "CHENNAI"
+            }
+        cy.request({
+            method: "POST",
+            url: "http://restapi.adequateshop.com/api/Tourist",
+            body:requestBody
+        })
+        .then((res) =>{
+            expect(res.status).to.eq(201)
+            expect(res.body.tourist_name).to.eq(requestBody.tourist_name)
+            expect(res.body.tourist_email).to.eq(requestBody.tourist_email)
+            expect(res.body.tourist_location).to.eq(requestBody.tourist_location)
         })
     })
 })

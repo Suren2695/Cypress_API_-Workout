c761ea87159b1b69f964bc69891a443fd48390da
Suren2695 <surensura46@gmail.com>
2023-09-08 23:40:14 +0530

    Headers and Cookies

---
+++ cypress/e2e/APITesting/HeadersAndCookies.cy.js
@@ -0,0 +1,20 @@
+// Get token, Submit Token, Get Order -> please refer the API collections.
+
+describe('Headers and Cookies - API Testing', () =>{
+
+    before('Creating access token', ()=>{
+
+        cy.request({
+            method: 'POST',
+            url:'https://simple-books-api.glitch.me/api-clients/',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: {
+                clientName:'ABC',
+                clientEmail:Math.random().toString(5).substring(2)+'gmail.com'
+            }
+        })
+    })
+
+})
